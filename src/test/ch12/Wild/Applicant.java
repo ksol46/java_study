98c128b67d37a47216da6d78f653c63928a63e52
ksol46 <doomee880406@naver.com>
2022-11-30 13:37:08 +0900

    d

---
+++ src/test/ch12/Wild/Applicant.java
@@ -0,0 +1,9 @@
+package test.ch12.Wild;
+
+public class Applicant <T> {
+	 public T kind;
+	   //생성자
+	   public Applicant(T kind) {
+	      this.kind = kind;
+	   }
+}
---
+++ src/test/ch12/Wild/Person.java
@@ -0,0 +1,19 @@
+package test.ch12.Wild;
+
+public class Person {}
+	 class Worker extends Person {}
+	   class Student extends Person {}
+	   class HighStudent extends Student {}
+	   class MiddleStudent extends Student {}
+
+/* 구조
+				Person
+			ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ	
+ 			|			|
+ 		Worker        Student
+ 					ㅡㅡㅡㅡㅡㅡㅡㅡ
+ 					|		  |
+ 				HighStu	     MiddleStu
+ 
+ */
+	   
